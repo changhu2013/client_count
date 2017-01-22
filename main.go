d45629d9254e7c704d7152b8e4d56d578f00b0bf
changhu <o52tiger@gmail.com>
2017-01-22 22:11:06 +0800

    重构代码

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
-import "github.com/garyburd/redigo/redis"
-import "strconv"
-import "os"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
 
 func getRedisConnect(redisURL string) redis.Conn {
 	conn, err := redis.DialURL(redisURL)
@@ -28,17 +35,15 @@ func doGetClientCount(c redis.Conn, key string) int64 {
 	return cc
 }
 
-func getClientCount(redisURL string, vvv bool) int64 {
-	var conn = getRedisConnect(redisURL)
+func getClientCount(conn redis.Conn, vvv bool) int64 {
 	var keys, err = redis.Strings(conn.Do("KEYS", "websocket_clients_count_*"))
-	defer conn.Close()
 
 	if err != nil {
 		fmt.Println(0)
-		return 0
 	}
 
 	var count int64
+
 	for idx := range keys {
 		var key = keys[idx]
 		var cc = doGetClientCount(conn, keys[idx])
@@ -53,17 +58,46 @@ func getClientCount(redisURL string, vvv bool) int64 {
 	return count
 }
 
+func doReport(count int64) {
+	var client = &http.Client{}
+	var json = "{\"time\":\"" + time.Now().String() + "\", \"count\":" + strconv.FormatInt(count, 10) + "}"
+	req, err := http.NewRequest("POST", "http://localhost:9090", bytes.NewBufferString(json))
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		bodystr := string(body)
+		fmt.Println(bodystr)
+	}
+}
+
 func main() {
 	var args = os.Args[1:]
 	var vvv = len(args) > 0 && args[0] == "-v"
 	var dev = len(args) > 1 && args[1] == "-d"
 
 	var redisURL = "redis://192.168.1.152:6379"
+
 	if dev {
 		redisURL = "redis://192.168.200.50:6379"
 	}
 
-	var count = getClientCount(redisURL, vvv)
+	var conn = getRedisConnect(redisURL)
+	defer conn.Close()
 
-	fmt.Println(count)
+start:
+
+	doReport(getClientCount(conn, vvv))
+	time.Sleep(time.Second)
+
+	goto start
+
 }
